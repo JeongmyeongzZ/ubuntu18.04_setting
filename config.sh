91de203374a29d48d83a5c4e6fc265ce25628fcb
jeongmyeong <kimbym23@gmail.com>
2019-10-20 22:03:40 +0900

    add composer install

--- config.sh
+++ config.sh
@@ -15,11 +15,24 @@ sudo apt-get install nimf nimf-libhangul fonts-nanum fonts-nanum-coding fonts-na
 # Git
 sudo apt-get install -y git
 
+# Composer
+sudo apt update
+sudo apt install curl php-cli php-mbstring git unzip -y
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
 # Docker
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo usermod -aG docker ${USER}
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo apt-get install docker-compose -y
 
 
 reboot
